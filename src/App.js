b8051012f8ce0833feb35416bc866d308fc8a3b0
Zefanja Timorason <z.timorason@gmail.com>
2023-02-18 16:26:39 +0100

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import contacts from "./contacts.json";
 import './App.css';
+import { useState } from "react";
 
 function App() {
+    const [contact, setContact] = useState(contacts)
+  
+  const slicedContacts = contacts.slice(0, 5)
+  
+  // function RandomContact() {
+  //   const [contact, setContact] = useState(contacts)
+
+  //   function newRandomContact(contact) {
+  //     return contact.floor[(Math.random()*contact.length)]
+  //   }
+  //   setContact(newRandomContact)
+    
+    // const newRandomContact = contactId => {
+    //   return contactid[Math.floor(Math.random()*contactId.length)]
+    // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Contact list</h2>
+      {/* <button onClick={RandomContact}>Add new random contact</button> */}
+      {slicedContacts.map(contact => {
+        return (
+          <table>
+        <li>Name:{contact.name}</li>
+        <li>Picture:<img src={contact.pictureUrl} alt='logo'/></li>
+        <li>Popularity:{contact.popularity}</li>
+        
+      </table>
+        )
+      })}
+      
     </div>
   );
 }
